7d8d54b7e33d0b8ac3f366ebd9d8b9cc1b71b426
AtulSikarwal <atulsikarwal5.com>
2022-06-18 09:47:11 +0530

    The code for  add Contacts in Address Book

---
+++ AddContactAddressBook/AddressBook.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystem
+{
+    class AddressBook
+    {
+
+        public static List<Contacts> Info = new List<Contacts>();
+        public void AddPerson()
+        {
+            Contacts Person = new Contacts();
+
+            Console.WriteLine("Enter the First Name :");
+            Person.FirstName = Console.ReadLine();
+
+            Console.WriteLine("Enter the Last Name :");
+            Person.LastName = Console.ReadLine();
+
+
+            Console.WriteLine("Enter the City Name :");
+            Person.City = Console.ReadLine();
+
+
+            Console.WriteLine("Enter the Zip Code :");
+            Person.Zip = Console.ReadLine();
+
+
+
+            Console.WriteLine("Enter the Email id :");
+            Person.Email = Console.ReadLine();
+
+
+            Console.WriteLine("Enter the Address :");
+            Person.Address = Console.ReadLine();
+
+            Info.Add(Person);
+
+        }
+
+
+        
+
+
+      
+    }
+}
